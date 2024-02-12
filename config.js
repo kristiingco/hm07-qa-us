09cfbb89c5a4ab84f98c4787a5327e78622ce2b5
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-02-11 20:48:46 -0800

    Add API url to config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-e675ec0d-ca8a-45f4-ae8d-b23f2c77bd5c.containerhub.tripleten-services.com"
 }
