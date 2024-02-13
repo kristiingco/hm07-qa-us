7c286649275ed8bed82f06aa3cf929680ce7737e
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-02-13 12:23:40 -0800

    Update server URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-a85385b6-c262-483e-85ef-f9fce6119a56.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-2310a5ef-698c-4f3b-b36c-4221f2654427.containerhub.tripleten-services.com"
 }
