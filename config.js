d79d6093bc9642edd3294a424643b3f778485f7a
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-02-12 22:20:49 -0800

    Add tests to putHandlers suite

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-606c7232-0d9e-47f6-8cff-25d8198c5356.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-a7f6a81e-ce57-45f3-8204-a6a3e850aac4.containerhub.tripleten-services.com"
 }
--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -1,11 +1,34 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-const requestBody = {}
-
-test('', async () => {
+//Grocery item price change (successful)
+test('status should be 200', async () => {
+	const requestBody = {
+		"price": 175
+	}
+	let resultStatus;
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/products/3`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		resultStatus = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+	expect(resultStatus).toBe(200);
+});
+
+test('should have an ok property with value set to true', async () => {
+	const requestBody = {
+		"price": 175
+	}
+	let response;
+    try {
+		response = await fetch(`${config.API_URL}/api/v1/products/3`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
@@ -15,4 +38,201 @@ test('', async () => {
 	} catch (error) {
 		console.error(error);
 	}
+	const data = await response.json();
+	expect(data).toHaveProperty("ok", true);
+});
+
+//Grocery item price change (product not found)
+test('status should be 404', async () => {
+	const requestBody = {
+		"price": 175
+	}
+	let resultStatus;
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/products/310`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		resultStatus = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+	expect(resultStatus).toBe(404);
+});
+
+test('should have a message property', async () => {
+	const requestBody = {
+		"price": 175
+	}
+	let response;
+    try {
+		response = await fetch(`${config.API_URL}/api/v1/products/310`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+	} catch (error) {
+		console.error(error);
+	}
+	const data = await response.json();
+	expect(data).toHaveProperty("message", "Not Found");
+});
+
+//Changing the kit (successful)
+test('status should be 200', async () => {
+	const requestBody = {
+		"name": "My modified kit",
+		"productsList": [
+			{
+				"id": 1,
+				"quantity": 4
+			},
+			{
+				"id": 5,
+				"quantity": 2
+			},
+			{
+				"id": 3,
+				"quantity": 1
+			},
+			{
+				"id": 4,
+				"quantity": 1
+			}
+		]
+	}
+	let resultStatus;
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/5`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		resultStatus = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+	expect(resultStatus).toBe(200);
+});
+
+test('should have an ok property with value set to true', async () => {
+	const requestBody = {
+		"name": "My modified kit",
+		"productsList": [
+			{
+				"id": 1,
+				"quantity": 4
+			},
+			{
+				"id": 5,
+				"quantity": 2
+			},
+			{
+				"id": 3,
+				"quantity": 1
+			},
+			{
+				"id": 4,
+				"quantity": 1
+			}
+		]
+	}
+	let response;
+    try {
+		response = await fetch(`${config.API_URL}/api/v1/kits/5`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+	} catch (error) {
+		console.error(error);
+	}
+	const data = await response.json();
+	expect(data).toHaveProperty("ok", true);
+});
+
+//Grocery item price change (kit not found)
+test('status should be 404', async () => {
+	const requestBody = {
+		"name": "My modified kit",
+		"productsList": [
+			{
+				"id": 1,
+				"quantity": 4
+			},
+			{
+				"id": 5,
+				"quantity": 2
+			},
+			{
+				"id": 3,
+				"quantity": 1
+			},
+			{
+				"id": 4,
+				"quantity": 1
+			}
+		]
+	}
+	let resultStatus;
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/52`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		resultStatus = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+	expect(resultStatus).toBe(404);
+});
+
+test('should have a message property', async () => {
+	const requestBody = {
+		"name": "My modified kit",
+		"productsList": [
+			{
+				"id": 1,
+				"quantity": 4
+			},
+			{
+				"id": 5,
+				"quantity": 2
+			},
+			{
+				"id": 3,
+				"quantity": 1
+			},
+			{
+				"id": 4,
+				"quantity": 1
+			}
+		]
+	}
+	let response;
+    try {
+		response = await fetch(`${config.API_URL}/api/v1/kits/52`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+	} catch (error) {
+		console.error(error);
+	}
+	const data = await response.json();
+	expect(data).toHaveProperty("message", "Not Found");
 });
