fcd7f8a6762d6c1c2e009cf981a9dfd9f1c4e81b
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-02-12 19:48:29 -0800

    Remove empty test in deleteHandlers suite

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,16 +1,6 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
-    try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
-			method: 'DELETE',
-		});
-	} catch (error) {
-		console.error(error);
-	}
-});
-
 // Deleting a kit (successful)
 test('status should be 200', async () => {
 	let resultStatus;
